7f6e3bbe6d6a3d5208b635f7fe6aae7da1f8967d
Readmore Yejoong <113605837+YejoongKwon@users.noreply.github.com>
2024-06-16 10:13:09 +0900

    Hadoop_Docker:create

--- docker-build.sh
+++ docker-build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-docker-compose down
+# docker-compose down
 
-docker build -t language-base ./languagebase
+docker build -t language-base ./language-base
 docker build -t hadoop-base ./hadoopbase
 docker build -t hadoop-spark-base ./hadoopsparkbase
 
 docker build -t hadoop-namenode ./namenode
 docker build -t hadoop-datanode ./datanode
-docker build -t resourcemanager ./hadoop/resourcemanager
-docker build -t yarn-timelineserver ./hadoop/yarntimelineserver
-docker build -t spark-historyserver ./hadoop/sparkhistoryserver
-docker build -t zeppelin ./hadoop/zeppelin
+docker build -t resourcemanager ./resourcemanager
+docker build -t yarn-timelineserver ./yarntimelineserver
+docker build -t spark-historyserver ./sparkhistoryserver
+docker build -t zeppelin ./zeppelin
 
 docker-compose up -d
